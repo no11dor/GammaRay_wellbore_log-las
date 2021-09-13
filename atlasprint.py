be0dc550d5f5fb8ef36298dcd3097b102d3c1c32
no11dor <90152192+no11dor@users.noreply.github.com>
2021-09-13 09:19:12 +0600

    Update atlasprint.py

--- atlasprint.py
+++ atlasprint.py
@@ -14,6 +14,7 @@ import threading
 import itertools
 from numpy import mean
 from numpy import std
+import PyPDF2
 
 
 window = Tk()
@@ -57,87 +58,91 @@ def open_file():
 
 def save_las():
     label8.config(text='wait...')
-    try:
 
-        global data
-        with open(file_path, 'r') as f:
-            first_data = f.read()
-        # replace mnemonics txt file
-        new_data2 = first_data.replace('TVD', 'TVDSS')
-        new_data3 = new_data2.replace('True Vertical Depth', 'True Vertical Depth Sub Sea')
-        new_data4 = new_data3.replace('TRUE VERTICAL DEPTH', 'True Vertical Depth Sub Sea')
-        new_data5 = new_data4.replace('VERTICAL DEPTH', 'True Vertical Depth Sub Sea')
-        new_data6 = new_data5.replace('VERTICALDEPTH', 'TVDSS')
-        new_data7 = new_data6.replace('GAMMA RAY', 'Gamma Ray')
-        new_data8 = new_data7.replace('GAMMA', 'GR')
-        new_data9 = new_data8.replace('API corrected gamma counts at sensor depth.', 'Gamma Ray')
-        new_data10 = new_data9.replace('GAMA', 'GR')
-        new_data11 = new_data10.replace('MECHANICAL SPEED', 'Rate Of Penetration')
-        new_data12 = new_data11.replace('MECHSPEED', 'ROP')
-        new_data13 = new_data12.replace('RATE OF PENERATION', 'Rate Of Penetration')
-        new_data14 = new_data13.replace('Rate of penetration at bit depth.', 'Rate Of Penetration')
-        new_data15 = new_data14.replace('DEPT', 'DEPTH')
-        new_data16 = new_data15.replace('DEPTHH', 'DEPTH')
-        new_data17 = new_data16.replace('GRCX', 'GR')
-        new_data18 = new_data17.replace('TVDSSSS', 'TVDSS')
-        new_data19 = new_data18.replace('MD', 'DEPTH')
-        # read data usig lasio
-        data = lasio.read(new_data19)
-        # data to pandas data frame, rename and sort columns
-        df = data.df()
-        # sort curves dataframe
-        df = df[['GR', 'TVDSS', 'ROP']]
-        global tvdss
-        tvdss = df['TVDSS']
-        #remouve outliers
-        upperlimit = df['GR'].mean() + 3 * df['GR'].std()
-        lowerlimit = df['GR'].mean() - 3 * df['GR'].std()
-        df['GR'] = np.where(df['GR'] > upperlimit, upperlimit, np.where(df['GR'] < lowerlimit, lowerlimit, df['GR']))
-        #approximate by moving average (hueta)
-        # df['GR'] = df['GR'].rolling(window=3).median()
-        #interpolate
-        df['GR'] = df['GR'].interpolate(method='cubic')
-        #smooth curves
-        from scipy.signal import savgol_filter
-        df['GR'] = savgol_filter(df['GR'], window_length = 21, polyorder = 13)
-        df['ROP'] = savgol_filter(df['ROP'], window_length=21, polyorder=5)
-        # pandas to data, write las
-        data.set_data(df)
-        data.write('C:\\AtlasPrint\\TEMP\\LASFILE.las', version=2.0)
-        # read las as txt for rename columns
-        with open('C:\\AtlasPrint\\TEMP\\LASFILE.las', 'r') as f:
-            renamecolumn = f.read()
-        renamecolumn1 = renamecolumn.replace('''DEPTH.M       : DEPTH
-    GR   .M       : True Vertical Depth Sub Sea
-    TVDSS.API     : Gamma Ray
-    ROP  .M.HOUR  : Rate Of Penetration''', '''DEPTH.M    001 : Measured Depth
-    GR   .API  --- : Gamma Ray
-    TVDSS.M    --- : True Vertical Depth Sub Sea
-    ROP  .M/HR --- : Rate Of Penetration''')
-        renamecolumn2 = renamecolumn1.replace('''DEPTH.M     : 1  MEASURED DEPTH
-    GR   .AAPI  : 2  Gamma Ray
-    TVDSS.M     : 3  True Vertical Depth Sub Sea
-    ROP  .M/H   : 4  Rate Of Penetration''', '''DEPTH.M    001 : Measured Depth
-    GR   .API  --- : Gamma Ray
-    TVDSS.M    --- : True Vertical Depth Sub Sea
-    ROP  .M/HR --- : Rate Of Penetration''')
-        renamecolumn3 = renamecolumn2.replace('''DEPTH.m    : DEPTH
-    GR   .m/h  : RATE OF PENETRATION
-    TVDSS.API  : Gamma Ray BOREHOLE CORRECTED
-    ROP  .m    : True Vertical Depth Sub Sea SUBSEA''', '''DEPTH.M    001 : Measured Depth
-        GR   .API  --- : Gamma Ray
-        TVDSS.M    --- : True Vertical Depth Sub Sea
-        ROP  .M/HR --- : Rate Of Penetration''')
-        # read txt lasio and save as las
-        renamecolumn4 = lasio.read(renamecolumn3)
-        renamecolumn4.write('C:\\AtlasPrint\\TEMP\\LASFILE.las', version=2.0)
-        lasname = filedialog.asksaveasfilename(title=u'save file ', filetypes=[(".las", ".las")])
-        renamecolumn4.write(lasname, version=2.0)
-        label8.config(text='.las saved')
 
-    except Exception as e:
-        messagebox.showinfo('Ошибка .las файла', e)
-        label8.config(text='ERROR')
+    global data
+    with open(file_path, 'r') as f:
+        first_data = f.read()
+    # replace mnemonics txt file
+    new_data1 = first_data.replace('TVD', 'TVDSS')
+    new_data2 = new_data1.replace('VERTICALDEPTH', 'TVDSS')
+    new_data3 = new_data2.replace('GAMMA', 'GR')
+    new_data4 = new_data3.replace('GAMA', 'GR')
+    new_data5 = new_data4.replace('MECHSPEED', 'ROP')
+    new_data6 = new_data5.replace('DEPT', 'DEPTH')
+    new_data7 = new_data6.replace('DEPTHH', 'DEPTH')
+    new_data8 = new_data7.replace('GRCX', 'GR')
+    new_data9 = new_data8.replace('TVDSSSS', 'TVDSS')
+    new_data10 = new_data9.replace('MD', 'DEPTH')
+    # read data usig lasio
+    data = lasio.read(new_data10)
+    # data to pandas data frame, rename and sort columns
+    df = data.df()
+    # sort curves dataframe
+    df = df[['GR', 'TVDSS', 'ROP']]
+    global tvdss
+    tvdss = df['TVDSS']
+    #remouve outliers
+    upperlimit = df['GR'].mean() + 3 * df['GR'].std()
+    lowerlimit = df['GR'].mean() - 3 * df['GR'].std()
+    df['GR'] = np.where(df['GR'] > upperlimit, upperlimit, np.where(df['GR'] < lowerlimit, lowerlimit, df['GR']))
+    #approximate by moving average (hueta)
+    # df['GR'] = df['GR'].rolling(window=3).median()
+    #interpolate
+    df['GR'] = df['GR'].interpolate(method='cubic')
+    #smooth curves
+    from scipy.signal import savgol_filter
+    df['GR'] = savgol_filter(df['GR'], window_length = 21, polyorder = 13)
+    df['ROP'] = savgol_filter(df['ROP'], window_length=21, polyorder=5)
+    # pandas to data, write las
+    data.set_data(df)
+    data.write('C:\\AtlasPrint\\TEMP\\LASFILE.las', version=2.0)
+
+    #writelines
+    with open('C:\\AtlasPrint\\TEMP\\LASFILE.las', 'r+') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith('DEPT'):
+                lines[i] = 'DEPTH.M    001 : Measured Depth                                     \n'
+        f.seek(0)
+        for line in lines:
+            f.write(line)
+
+    with open('C:\\AtlasPrint\\TEMP\\LASFILE.las', 'r+') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith('GR'):
+                lines[i] = 'GR   .API  --- : Gamma Ray                       \n'
+        f.seek(0)
+        for line in lines:
+            f.write(line)
+
+    with open('C:\\AtlasPrint\\TEMP\\LASFILE.las', 'r+') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith('TVDSS'):
+                lines[i] = 'TVDSS.M    --- : True Vertical Depth Sub Sea                            \n'
+        f.seek(0)
+        for line in lines:
+            f.write(line)
+
+    with open('C:\\AtlasPrint\\TEMP\\LASFILE.las', 'r+') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith('ROP'):
+                lines[i] = 'ROP  .M/HR --- : Rate Of Penetration                           \n'
+        f.seek(0)
+        for line in lines:
+            f.write(line)
+
+    with open('C:\\AtlasPrint\\TEMP\\LASFILE.las', 'r') as f:
+        finaltxt = f.read()
+    # read txt lasio and save as las
+    lasfinal = lasio.read(finaltxt)
+    lasfinal.write('C:\\AtlasPrint\\TEMP\\LASFILE.las', version=2.0)
+    lasname = filedialog.asksaveasfilename(title=u'save las file ', filetypes=[(".las", ".las")])
+    lasfinal.write(lasname, version=2.0)
+    label8.config(text='.las saved')
 
 
 def save_md():
@@ -246,9 +251,23 @@ def save_md():
         merger = PdfFileMerger()
         for pdf in pdfs:
             merger.append(pdf)
-        pdfmdname = filedialog.asksaveasfilename(title=u'save file ', filetypes=[(".pdf", ".pdf")])
+        pdfmdname = filedialog.asksaveasfilename(title=u'save MD file ', filetypes=[(".pdf", ".pdf")])
         merger.write(pdfmdname)
         merger.close()
+        label8.config(text='wait...')
+
+        #scale pdf pages
+        pdf = pdfmdname
+        pdf = PyPDF2.PdfFileReader(pdf)
+        page0 = pdf.getPage(0)
+        page1 = pdf.getPage(1)
+        page0.scaleBy(0.5)  # float representing scale factor - this happens in-place
+        page1.scaleBy(0.5)  # float representing scale factor - this happens in-place
+        writer = PyPDF2.PdfFileWriter()  # create a writer to save the updated results
+        writer.addPage(page0)
+        writer.addPage(page1)
+        with open(pdfmdname, "wb+") as f:
+            writer.write(f)
         label8.config(text='md saved')
     except Exception as e:
         messagebox.showinfo('Ошибка MD.pdf', e)
@@ -339,9 +358,25 @@ def save_tvdss():
         merger = PdfFileMerger()
         for pdf in pdfs:
             merger.append(pdf)
-        pdftvdssname = filedialog.asksaveasfilename(title=u'save file ', filetypes=[(".pdf", ".pdf")])
+        pdftvdssname = filedialog.asksaveasfilename(title=u'save tvdss file ', filetypes=[(".pdf", ".pdf")])
         merger.write(pdftvdssname)
         merger.close()
+        label8.config(text='wait...')
+
+        # scale pdf pages
+        pdf = pdftvdssname
+        pdf = PyPDF2.PdfFileReader(pdf)
+        page0 = pdf.getPage(0)
+        page1 = pdf.getPage(1)
+        page0.scaleBy(0.5)  # float representing scale factor - this happens in-place
+        page1.scaleBy(0.5)  # float representing scale factor - this happens in-place
+        writer = PyPDF2.PdfFileWriter()  # create a writer to save the updated results
+        writer.addPage(page0)
+        writer.addPage(page1)
+        with open(pdftvdssname, "wb+") as f:
+            writer.write(f)
+        label8.config(text='tvdss saved')
+        time.sleep(2)
         label8.config(text='')
 
     except Exception as e:
